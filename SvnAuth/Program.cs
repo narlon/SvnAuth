631b568d2dafa070a6c669c37acbd0442eaad1e8
narlon <narlonzjh@163.com>
2017-09-27 13:21:26 +0800

    add the codes

---
+++ SvnAuth/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using OfficeOpenXml;
+
+namespace SvnAuth
+{
+    class Program
+    {
+        private static bool hasWriteHead = false;
+
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("./SvnAuth.xlsx");
+            var ep = new ExcelPackage(file);
+            ExcelWorkbook workbookIn = ep.Workbook;
+
+            StreamWriter sw = new StreamWriter("./dav_svn_st.authz");
+            foreach (var sheet in workbookIn.Worksheets)
+            {
+                CheckSheet(sw, sheet);
+            }
+
+            sw.Close();
+        }
+
+        private static void CheckSheet(StreamWriter sw, ExcelWorksheet sheet)
+        {
+            int collumCount = sheet.Dimension.End.Column;
+            int rowCount = sheet.Dimension.End.Row;
+            if (!hasWriteHead)
+            {
+                sw.WriteLine("[groups]");
+                string groupName = "";
+                List<string> groupMember = new List<string>();
+                for (int i = 2; i <= collumCount; i++) //写组信息
+                {
+                    string val = sheet.GetValue(1, i).ToString();
+                    if (val.StartsWith("@")) //是一个组
+                    {
+                        if (groupName != "") //把上一个组信息写入文件
+                        {
+                            sw.WriteLine(string.Format("{0}={1}", groupName.Substring(1), string.Join(",", groupMember.ToArray())));
+                        }
+                        groupName = val;
+                        groupMember.Clear();
+                    }
+                    else
+                    {
+                        groupMember.Add(val);
+                    }
+                }
+                if (groupName != "") //把最后一个组信息写入文件
+                {
+                    sw.WriteLine(string.Format("{0}={1}", groupName.Substring(1), string.Join(",", groupMember.ToArray())));
+                }
+                hasWriteHead = true;
+            }
+
+            sw.WriteLine();
+            for (int j = 2; j <= rowCount; j++) //遍历其他所有行
+            {
+                string dirName = sheet.GetValue(j, 1).ToString(); //读取路径名字
+                sw.WriteLine("[{0}]", dirName);
+                sw.WriteLine("* =");
+                for (int i = 2; i <= collumCount; i++) //写组信息
+                {
+                    var valObj = sheet.GetValue(j, i);
+                    if (valObj != null) //说明有某种权限
+                    {
+                        string opName = sheet.GetValue(1, i).ToString();
+                        sw.WriteLine("{0} = {1}", opName, valObj.ToString());
+                    }
+                }
+                sw.WriteLine();
+            }
+        }
+    }
+}
---
+++ SvnAuth/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SvnAuth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SvnAuth")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4d73100-fd74-4d09-a8cc-98d619ccab60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
